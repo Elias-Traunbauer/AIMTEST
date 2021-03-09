d9d963cd1912699591ac7f74524e4e29cad76599
GottaCode-Unity <d>
2021-03-09 11:26:25 +0100

    Projektdateien hinzufügen.

---
+++ AIMTEST/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AIMTEST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+
+            button1.Location = new Point(rd.Next(200, 1000), rd.Next(100, 500));
+        }
+    }
+}
